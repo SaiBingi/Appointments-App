4d1bd6b7a53c483d16a872660539172b8e75d21c
SaiBingi <saibingi1208@gmail.com>
2022-05-01 19:23:40 +0000

    Appointment App Solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Published URL ==>  https://myappointmentap.ccbp.tech
+
+import Appointments from './components/Appointments'
+
+import './App.css'
+
+const App = () => <Appointments />
+
+export default App
